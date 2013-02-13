50b988e9b43ce53ea52e2fc8478f9f382e88bfe5
Evan Rosen <erosen@wikimedia.org>
2013-02-13 22:50:33 +0000

    adds support for generating graphs for the total number of revisions by cohort

--- historical/process_pd.py
+++ historical/process_pd.py
@@ -35,31 +35,39 @@ val_keys = ['total_editors',
               'new_editors',
               'active_editors', 
               'very_active_editors',
+              'num_articles',
               'new_articles_per_day', 
               'mean_edits',
+              'db_size',
               'num_links_internal', 
               'num_links_interwiki',
               'num_links_image', 
               'num_links_external',
               'num_links_redirects']
 
-df = pd.read_table(stats_fn, sep=',', header=None, parse_dates=['date'], names=fieldnames)
+df_long = pd.read_table(stats_fn, sep=',', header=None, parse_dates=['date'], names=fieldnames)
+
+print df_long
 
 indic_lang_df = pd.read_table('../data/indic_lang_ids.tsv', sep='\t', comment='#', names=['id','name'])
 indic_langs = indic_lang_df['id'].dropna().unique()
 
 for val_key in val_keys:
     print 'processing val_key: %s' % val_key
-    pt = df.pivot(index='date', columns='project', values=val_key)
+    df = df_long.pivot(index='date', columns='project', values=val_key)
     # print pt
     limn_id = 'overall_%s' % val_key
     limn_title = limn_id.replace('_', ' ').title()
-    ds = limnpy.DataSource(limn_id, limn_title, pt)
+    ds = limnpy.DataSource(limn_id, limn_title,df)
     ds.write(basedir)
 
-    indic_pt = pd.DataFrame(pt[indic_langs].sum(axis=1), columns=[val_key])
-    # print indic_pt
+
+
+    indic_df = df[indic_langs]
+    indic_df['Total'] = indic_df.sum(axis=1)
     indic_limn_id = 'indic_language_%s' % val_key
     indic_limn_title = indic_limn_id.replace('_', ' ').title()
-    indic_ds = limnpy.DataSource(indic_limn_id, indic_limn_title, indic_pt)
+    indic_ds = limnpy.DataSource(indic_limn_id, indic_limn_title, indic_df)
+    # print indic_pt
     indic_ds.write(basedir)
+    indic_ds.write_graph(indic_langs, basedir=basedir)
